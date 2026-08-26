6ec987a9dcd5034b62caede9c42df49ecc2d0623
katemzr <katemzr@gmail.com>
2026-08-26 13:38:05 -0500

    initial commit

---
+++ src/main/java/com/launchcode/practice_github_pilot_group/PracticeGithubPilotGroupApplication.java
@@ -0,0 +1,13 @@
+package com.launchcode.practice_github_pilot_group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeGithubPilotGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeGithubPilotGroupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/launchcode/practice_github_pilot_group/PracticeGithubPilotGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.launchcode.practice_github_pilot_group;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeGithubPilotGroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
